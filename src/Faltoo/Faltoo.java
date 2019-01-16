118ce19c20f9c1a933614574b10b2f1efb0002f1
hcool2489 <hcool2489@gmail.com>
2019-01-16 11:33:11 +0530

    Initial commit

---
+++ src/Faltoo/Faltoo.java
@@ -0,0 +1,23 @@
+package Faltoo;
+
+import java.util.Arrays;
+
+public class Faltoo {
+    int[] list = new int[10];
+    
+    public static void main(String[] args) {
+        Faltoo l1 = new Faltoo();
+        System.out.println(Arrays.toString(l1.list));
+        l1.insert(42);
+        System.out.println(Arrays.toString(l1.list));
+    }
+    
+    void insert(int value){
+        for (int i = 0; i < list.length; i++) {
+            if(list[i] == 0){
+                list[i] = value;
+                break;
+            }
+        }
+    }
+}
